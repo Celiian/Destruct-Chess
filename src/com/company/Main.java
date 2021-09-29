d96ac7c5b24884d361615e8f76b51705a7727a54
Celian <celian.opigez@edu.esiee-it.fr>
2021-09-29 11:42:55 +0200

    src/com/company/Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,19 +7,22 @@ public class Main {
 
     static Scanner sc = new Scanner(System.in);
 
+
+    //Sous menu règle, permet de lire les règles du jeu avant de retourner au menu.
     public static void sousMenuRegle(){
         boolean fin = false;
         boolean continuer = true;
         while (continuer){
-
-
         boolean inputCorrect = true;
         int menu = 0;
         while (inputCorrect) {
             try {
-                System.out.println("\n\nLe but du jeu est de bloquer l'autre joueur. Pour se faire, deux actions sont effectués tour à tour par les joueurs :\n" +
-                    "1) Le joueur doit déplacer son pion, horizontalement ou verticalement \n" +
-                    "2) Le joueur doit choisir une case du plateau à détruire");
+                System.out.println("\n\nPendant son tour un joueur peut déplacer son pion d’une case (verticalement ou horizontalement), puis il détruit une case du plateau.\n" +
+                        "Le dernier joueur pouvant encore se déplacer gagne.\n" +
+                        "Contraintes :\n" +
+                        "- Un joueur ne peut pas détruire une case occupée.\n" +
+                        "- Un joueur ne peut pas occuper une case détruite ou une case déjà occupée.\n" +
+                        "- Un joueur bloqué pendant un tour est déclaré perdant.\n");
                 System.out.println("Retour : Tappez 3 ");
                 menu = sc.nextInt();
                 inputCorrect = false;
@@ -28,30 +31,23 @@ public class Main {
                 sc.next();
             }
         }
-
         if (menu == 3){
             menuPrincipal();
             continuer = false;
         }
         else {
-
-
         }
         }
     }
 
     public static void menuPrincipal() {
+        String[][] tableau = tableZero();
         boolean fin = false;
         while (fin == false) {
             int menu = 0;
             boolean inputCorrect = true;
-
-
-            System.out.println("Menu Principal : ");
-
-
+            System.out.println(" \n\nMenu Principal : ");
             while (menu != 4) {
-
                 while (inputCorrect) {
                     try {
                         System.out.println("Selectionnez une option :");
@@ -62,11 +58,12 @@ public class Main {
                         System.out.println(" \n Veuillez recommencer et choisir une option valide \n");
                         sc.next();
                     }
-
                 }
-
                 if (menu == 1) {
-
+                    Jeu();
+                    fin = true;
+                    menu = 4;
+                    inputCorrect = true;
                 } else if (menu == 2) {
 
                 } else if (menu == 3) {
@@ -75,6 +72,7 @@ public class Main {
                 }
                 else if (menu == 4) {
                     fin = true;
+                    menu = 4;
                 }
                 else {
                     System.out.println(" \n Veuillez recommencer et choisir une option valide \n");
@@ -83,20 +81,71 @@ public class Main {
                     menu = sc.nextInt();
                     break;
                 }
-
             }
         }
     }
 
 
+
+    public static void affichageTableau(String tableau[][]) {
+        for (int j = 0; j < 11; j++) {
+            System.out.print( j );
+            System.out.print( "  " );
+        }
+        System.out.println("");
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 11; j++) {
+                System.out.print(tableau[i][j] + " " );
+            }System.out.print(i);
+            System.out.println("");
+        }
+    }
+
+    public static String[][] tableZero() {
+        String[][] tableau = new String[10][11];
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 11; j++) {
+                if (i == 5 && j == 4){
+                    tableau[i][j] = "\uD83D\uDFE6";
+                }
+                else if (i == 5 && j == 5) {
+                    tableau[i][j] = "\uD83D\uDFE5";
+                }
+                else{
+                    tableau[i][j] = "⬜";
+                }
+            }
+        }
+        return tableau;
+    }
+
+
+    public static void Jeu(){
+        String[][] tableau = tableZero();
+        affichageTableau(tableau);
+
+
+    }
+
+
+    public static void deplacementJoueur(){
+        System.out.println("Déplacez votre pion : ");
+        System.out.println("Appuez sur 1 pour aller a gauche");
+        System.out.println("Appuez sur 2 pour aller en haut");
+        System.out.println("Appuez sur 3 pour aller en bas");
+        System.out.println("Appuez sur 4 pour aller a droite");
+        int déplacement = sc.nextInt();
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 11; j++) {
+                
+            }
+        }
+
+
+    }
+
     public static void main(String[] args) {
-
-
         menuPrincipal();
-
-
-
-
-
     }
 }
